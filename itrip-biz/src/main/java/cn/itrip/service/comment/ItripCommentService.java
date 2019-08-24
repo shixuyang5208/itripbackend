773f2f0c198f4adfee9721272fce411d0de280f5
xuyang.shi@hotmail.com <xuyang.shi@hotmail.com>
2019-08-24 21:01:24 +0800

    霸王花

--- itrip-biz/src/main/java/cn/itrip/service/comment/ItripCommentService.java
+++ itrip-biz/src/main/java/cn/itrip/service/comment/ItripCommentService.java
@@ -1,18 +1,20 @@
 package cn.itrip.service.comment;
-
 import cn.itrip.beans.pojo.ItripComment;
-import cn.itrip.beans.pojo.ItripImage;
-import cn.itrip.beans.vo.comment.ItripListCommentVO;
-import cn.itrip.beans.vo.comment.ItripScoreCommentVO;
-import cn.itrip.common.Page;
-
 import java.util.List;
 import java.util.Map;
 
+import cn.itrip.beans.pojo.ItripImage;
+import cn.itrip.beans.vo.comment.ItripScoreCommentVO;
+import cn.itrip.beans.vo.comment.ItripListCommentVO;
+import cn.itrip.common.Page;
+/**
+* Created by shang-pc on 2015/11/7.
+*/
 public interface ItripCommentService {
+
     public ItripComment getItripCommentById(Long id)throws Exception;
 
-    public List<ItripListCommentVO> getItripCommentListByMap(Map<String, Object> param)throws Exception;
+    public List<ItripListCommentVO>	getItripCommentListByMap(Map<String, Object> param)throws Exception;
 
     public Integer getItripCommentCountByMap(Map<String, Object> param)throws Exception;
 
@@ -32,7 +34,7 @@ public interface ItripCommentService {
     public Page<ItripListCommentVO> queryItripCommentPageByMap(Map<String, Object> param, Integer pageNo, Integer pageSize)throws Exception;
 
     /**
-     * 根据酒店的id查询并计算所有点评的位置、设施、服务、卫生和综合评分
+     * 根据酒店的id查询并计算所有点评的位置、设施、服务、卫生和综合评分-add by donghai
      * @param hotelId
      * @return
      * @throws Exception
--- itrip-biz/src/main/java/cn/itrip/service/comment/ItripCommentServiceImpl.java
+++ itrip-biz/src/main/java/cn/itrip/service/comment/ItripCommentServiceImpl.java
@@ -1,63 +1,89 @@
 package cn.itrip.service.comment;
-
-import cn.itrip.beans.pojo.ItripComment;
+import cn.itrip.beans.pojo.ItripHotelOrder;
 import cn.itrip.beans.pojo.ItripImage;
-import cn.itrip.beans.vo.comment.ItripListCommentVO;
 import cn.itrip.beans.vo.comment.ItripScoreCommentVO;
-import cn.itrip.common.Constants;
+import cn.itrip.beans.vo.comment.ItripListCommentVO;
+import cn.itrip.common.BigDecimalUtil;
+import cn.itrip.dao.comment.ItripCommentMapper;
+import cn.itrip.beans.pojo.ItripComment;
 import cn.itrip.common.EmptyUtils;
 import cn.itrip.common.Page;
-import cn.itrip.dao.comment.ItripCommentMapper;
+import cn.itrip.dao.hotelorder.ItripHotelOrderMapper;
+import cn.itrip.dao.image.ItripImageMapper;
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
-
 import javax.annotation.Resource;
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
-
+import cn.itrip.common.Constants;
 @Service
 public class ItripCommentServiceImpl implements ItripCommentService {
+    private Logger logger = Logger.getLogger(ItripCommentServiceImpl.class);
     @Resource
     private ItripCommentMapper itripCommentMapper;
     @Resource
-    private ItripComment itripComment;
+    private ItripImageMapper itripImageMapper;
     @Resource
-    private ItripScoreCommentVO itripScoreCommentVO;
+    private ItripHotelOrderMapper itripHotelOrderMapper;
 
-    @Override
-    public ItripComment getItripCommentById(Long id) throws Exception {
+
+    public ItripComment getItripCommentById(Long id)throws Exception{
         return itripCommentMapper.getItripCommentById(id);
     }
 
-    @Override
-    public List<ItripListCommentVO> getItripCommentListByMap(Map<String, Object> param) throws Exception {
+    public List<ItripListCommentVO> getItripCommentListByMap(Map<String,Object> param)throws Exception{
         return itripCommentMapper.getItripCommentListByMap(param);
     }
 
-    @Override
-    public Integer getItripCommentCountByMap(Map<String, Object> param) throws Exception {
+    public Integer getItripCommentCountByMap(Map<String,Object> param)throws Exception{
         return itripCommentMapper.getItripCommentCountByMap(param);
     }
 
-    @Override
-    public boolean itriptxAddItripComment(ItripComment obj, List<ItripImage> itripImages) throws Exception {
-
+    /**
+     * 新增评论信息-add by hanlu
+     * @param obj
+     * @param itripImages
+     * @return
+     * @throws Exception
+     */
+    public boolean itriptxAddItripComment(ItripComment obj, List<ItripImage> itripImages)throws Exception{
+        if(null != obj ){
+            //计算综合评分，综合评分=(设施+卫生+位置+服务)/4
+            float score = 0;
+            int sum = obj.getFacilitiesScore()+obj.getHygieneScore()+obj.getPositionScore()+obj.getServiceScore();
+            score = BigDecimalUtil.OperationASMD(sum,4, BigDecimalUtil.BigDecimalOprations.divide,1, BigDecimal.ROUND_DOWN).floatValue();
+            //对结果四舍五入
+            obj.setScore(Math.round(score));
+            Long commentId = 0L;
+            if(itripCommentMapper.insertItripComment(obj) > 0 ){
+                commentId = obj.getId();
+                logger.debug("新增评论id：================ " + commentId);
+                if(null != itripImages && itripImages.size() > 0 && commentId > 0){
+                    for (ItripImage itripImage:itripImages) {
+                        itripImage.setTargetId(commentId);
+                        itripImageMapper.insertItripImage(itripImage);
+                    }
+                }
+                //更新订单表-订单状态为4（已评论）
+                itripHotelOrderMapper.updateHotelOrderStatus(obj.getOrderId(),obj.getCreatedBy());
+                return true;
+            }
+        }
         return false;
     }
 
-    @Override
-    public Integer itriptxModifyItripComment(ItripComment itripComment) throws Exception {
+    public Integer itriptxModifyItripComment(ItripComment itripComment)throws Exception{
         itripComment.setModifyDate(new Date());
         return itripCommentMapper.updateItripComment(itripComment);
     }
 
-    @Override
-    public Integer itriptxDeleteItripCommentById(Long id) throws Exception {
+    public Integer itriptxDeleteItripCommentById(Long id)throws Exception{
         return itripCommentMapper.deleteItripCommentById(id);
     }
 
-    @Override
-    public Page<ItripListCommentVO> queryItripCommentPageByMap(Map<String, Object> param, Integer pageNo, Integer pageSize) throws Exception {
+    public Page<ItripListCommentVO> queryItripCommentPageByMap(Map<String,Object> param, Integer pageNo, Integer pageSize)throws Exception{
         Integer total = itripCommentMapper.getItripCommentCountByMap(param);
         pageNo = EmptyUtils.isEmpty(pageNo) ? Constants.DEFAULT_PAGE_NO : pageNo;
         pageSize = EmptyUtils.isEmpty(pageSize) ? Constants.DEFAULT_PAGE_SIZE : pageSize;
